7bac485e4b88d6ebe5fd6f7a635c6f5e75216d39
Ken Huffman <huffmancoding@gmail.com>
2019-01-06 15:14:11 -0500

    Changes for vscode usage

--- src/main/java/com/huffmancoding/kingdomino/IllegalMoveException.java
+++ src/main/java/com/huffmancoding/kingdomino/IllegalMoveException.java
@@ -2,6 +2,8 @@ package com.huffmancoding.kingdomino;
 
 public class IllegalMoveException extends Exception
 {
+    private static final long serialVersionUID = 1;
+    
     public IllegalMoveException(String message)
     {
         super(message);
